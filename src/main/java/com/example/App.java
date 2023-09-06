828e1e788d7f0a458e545013f1dc1900b0a7d31f
softspider <sftspider@gmail.com>
2023-09-06 23:56:15 +0300

    feat: everything works correctly

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,12 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello, World!");
     }
 }
